caa80129bca74c21f045ab97d2d726e304aa8daf
kandascan <boguslaw.boczkowski@gmail.com>
2018-05-20 14:38:06 +0200

    Created three layers architecture for solution

---
+++ WebDomain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebDomain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
