ca03142be714e4ea94dbfec2798154d680dabd5a
kandascan <boguslaw.boczkowski@gmail.com>
2018-05-22 22:50:15 +0200

    add migrations to dataaccess, created new mvc web app without authorization

---
+++ DataAccess/ScrumManagerDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DataAccess
+{
+    public class ApplicationContextDbFactory : IDesignTimeDbContextFactory<ScrumManagerDbContext>
+    {
+        ScrumManagerDbContext IDesignTimeDbContextFactory<ScrumManagerDbContext>.CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ScrumManagerDbContext>();
+            var connectionString = @"Data Source=DESKTOP-BVKHG2F;Initial Catalog=ScrumManager;Integrated Security=True";
+            optionsBuilder.UseSqlServer<ScrumManagerDbContext>(connectionString);
+            return new ScrumManagerDbContext(optionsBuilder.Options);
+        }
+    }
+    public class ScrumManagerDbContext : DbContext
+    {
+        public ScrumManagerDbContext(DbContextOptions<ScrumManagerDbContext> options)
+            : base(options){}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DataAccess/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataAccess
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id  { get; set; }
+        public string Username { get; set; }
+        public bool Active { get; set; }
+        public string Password { get; set; }
+    }
+}
