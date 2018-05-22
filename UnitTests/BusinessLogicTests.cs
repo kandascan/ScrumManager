534075290b44aa5db079ecc91065da115418a0a9
kandascan <boguslaw.boczkowski@gmail.com>
2018-05-23 00:26:14 +0200

    added unit tests for buisness logic

---
+++ UnitTests/BusinessLogicTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using BusinessLogic;
+using DataAccess;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class BusinessLogicTests
+    {
+        [Test]
+        public void ShouldAddNewUser()
+        {
+            var uow = new UnitOfWork();
+            var lastUserId = uow.Repository<User>().GetDetails(x => x.Id < 0);
+            var randomUser = GenerateUser(lastUserId.Id);
+            Assert.Greater(0, randomUser.Id);
+        }
+
+        private User GenerateUser(int id)
+        {
+            var rand = new Random();
+            int count = rand.Next(8, 20);
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                var asciNumber = rand.Next(97, 122);
+                byte b = Convert.ToByte(asciNumber);
+                str.Append(Encoding.ASCII.GetString(new[] {b}));
+            }
+
+            return new User
+            {
+                Id = id,
+                Active = true,
+                Password = "TestPass",
+                Username = str.ToString()
+            };
+        }
+    }
+}
